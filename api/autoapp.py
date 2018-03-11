9a3439922613008f92b0e048ab046f0c91b58a05
NikitaVysotskiy <fordf150svtraptor@gmail.com>
2018-03-11 23:00:27 +0200

    init layout

---
+++ api/autoapp.py
@@ -0,0 +1,3 @@
+from diesel.app import create_app
+
+app = create_app
---
+++ api/diesel/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+def create_app(config_object=None):
+    if config_object is None:
+        config_object = {}
+
+    print(__name__.split('.')[0])
+    app = Flask(__name__.split('.')[0])
+    app.url_map.strict_slashes = False
+    app.config.from_object(config_object)
+    return app
