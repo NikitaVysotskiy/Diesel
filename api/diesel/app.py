485ff2e0479b5b65db006763546675bcf7b99de7
NikitaVysotskiy <fordf150svtraptor@gmail.com>
2018-03-11 23:06:25 +0200

    api app init

--- api/diesel/app.py
+++ api/diesel/app.py
@@ -5,7 +5,6 @@ def create_app(config_object=None):
     if config_object is None:
         config_object = {}
 
-    print(__name__.split('.')[0])
     app = Flask(__name__.split('.')[0])
     app.url_map.strict_slashes = False
     app.config.from_object(config_object)
