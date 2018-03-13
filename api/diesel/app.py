a5b860a19cd1c419dd0ce6783e5792d74038ae93
Nikita Vysotskiy <nikita.vysotskiy@io-soft.com.ua>
2018-03-13 12:03:25 +0200

    pyjwt

--- api/diesel/app.py
+++ api/diesel/app.py
@@ -3,10 +3,7 @@ from flask import Flask
 from api.diesel.extensions import db
 
 
-def create_app(config_object=None):
-    if config_object is None:
-        config_object = {}
-
+def create_app(config_object):
     app = Flask(__name__.split('.')[0])
     app.url_map.strict_slashes = False
     app.config.from_object(config_object)
