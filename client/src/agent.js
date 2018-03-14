85089591fdb0077b716b769a598c43cdfda1bfdd
Nikita Vysotskiy <nikita.vysotskiy@io-soft.com.ua>
2018-03-14 18:27:06 +0200

    requests agent added

---
+++ client/src/agent.js
@@ -0,0 +1,30 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+
+import { API_ROOT } from 'constants/constants'
+
+const superagent = superagentPromise(_superagent, global.Promise);
+
+let token = null;
+const setToken = req => {
+    if (token) {
+        req.set('authorization', `Token ${token}`);
+    }
+};
+
+const getResBody = res => res.body;
+
+const requests = {
+    get: url => superagent.get(`${API_ROOT}${url}`).use(setToken).then(getResBody),
+    post: (url, body) => superagent.post(`${API_ROOT}${url}`, body).use(setToken).then(getResBody),
+    put: (url, body) => superagent.put(`${API_ROOT}${url}`, body).use(setToken).then(getResBody),
+    del: url => superagent.del(`${API_ROOT}${url}`).use(setToken).then(getResBody),
+};
+
+const Auth = {
+    register: (username, email, password) => requests.post('/users', {username, email, password}),
+    login: (email, password) => requests.post('/users/login', {email, password}),
+    get: () => requests.get('/user')
+};
+
+
